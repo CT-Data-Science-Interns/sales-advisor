c0b879aee2aad687e8bb3911dea478d0cf2f7e0f
Alquen Antonio Sarmiento <sarmientoalquen@gmail.com>
2024-04-01 09:32:33 +0800

    Itinerary Maker V1.0 (#11)
    * feat: add a form select component for multi and single select
    
    * feat: add a form text input component
    
    * feat: add an initial dynamic map that currently respond to click event and returns latLang
    
    * chore: add dependency for google maps api and select component
    
    * feat: add a component for tracking the current itinerary stage
    
    * feat: add a component for the first itinerary stage
    
    * feat: add a component for the second itinerary stage
    
    * feat: update the page to contain the components
    
    * chore: linting errors
    
    * fix: move api key to env

---
+++ app/dashboard/itinerary-maker/components/starting-location-form.tsx
@@ -0,0 +1,94 @@
+"use client";
+import DynamicMap from "@/components/dynamic-map";
+import FormTextInput from "@/components/form-text-input";
+
+import React, { FormEvent, useState } from "react";
+import { ItineraryStage } from "./itinerary-stage-progress";
+
+const StartingLocationForm = ({
+  currentPageHandler,
+}: {
+  currentPageHandler: CallableFunction;
+}) => {
+  // const [date, setDate] = useState<{
+  //   startDate: Date;
+  //   endDate: Date;
+  // } | null>(null);
+  // const [country, setCountry] = useState<string | null>(null);
+  // const [state, setState] = useState<string | null>(null);
+  const [address, setAddress] = useState<string | null>(null);
+  const [latLng, setLatLng] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
+
+  const handleFormSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    currentPageHandler(ItineraryStage.FILTER_COMPANIES);
+  };
+
+  // Logging for now to remove linting errors
+  console.log(address, latLng);
+
+  return (
+    <form action="#" onSubmit={handleFormSubmit}>
+      <div className="gap-4 sm:mb-2 sm:grid sm:grid-cols-2 sm:gap-6 xl:grid-cols-5">
+        <div className="space-y-4 rounded-xl border-2 border-gray-300 p-4 xl:col-span-2">
+          <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+            Indicate Starting Location
+          </h2>
+          {/* <div>
+            <div className="space-y-4 md:flex md:space-x-4 md:space-y-0">
+              <div className="w-full">
+                <FormSelect
+                  title="Country"
+                  onSelectChange={setCountry}
+                  options={[
+                    "Philippines",
+                    "United States",
+                    "United Kingdom",
+                    "Japan",
+                  ]}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="space-y-4 md:flex md:space-x-4 md:space-y-0">
+            <div className="w-full">
+              <FormSelect
+                title="State"
+                onSelectChange={setState}
+                options={["Pangasinan", "QC", "Manila", "Cebu"]}
+              />
+            </div>
+          </div> */}
+
+          <div className="space-y-4 md:flex md:space-x-4 md:space-y-0">
+            <div className="w-full">
+              <FormTextInput
+                title="Address"
+                value={address ?? ""}
+                onInputChange={setAddress}
+              />
+            </div>
+          </div>
+          <div>Latitude: {latLng?.lat}</div>
+          <div>Longitude: {latLng?.lng}</div>
+          <button
+            type="submit"
+            className="justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Proceed to Filtering
+          </button>
+        </div>
+
+        <div className="mb-4 space-y-4 xl:col-span-3">
+          <DynamicMap latLangHandler={setLatLng} addressHandler={setAddress} />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default StartingLocationForm;
