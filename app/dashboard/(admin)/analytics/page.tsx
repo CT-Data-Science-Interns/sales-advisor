33b209fb80a4c65af3fabc7d352e1f8a29dff487
JM Elbambo <jm.elbambo27@gmail.com>
2024-04-05 09:39:39 +0800

    Fix merge conflict issues

--- app/dashboard/(admin)/analytics/page.tsx
+++ app/dashboard/(admin)/analytics/page.tsx
@@ -6,12 +6,6 @@ import DatePicker from "tailwind-datepicker-react";
 import { IOptions } from "tailwind-datepicker-react/types/Options";
 
 import "svgmap/dist/svgMap.min.css";
-import React, { useEffect, useState } from "react";
-
-import DatePicker from "tailwind-datepicker-react";
-import { IOptions } from "tailwind-datepicker-react/types/Options";
-
-import "svgmap/dist/svgMap.min.css";
 
 const Page = () => {
   // let companiesCount: number = 0;
