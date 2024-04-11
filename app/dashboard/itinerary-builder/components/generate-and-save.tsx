c05c0fa45d228bec5b98b1001149cd58e5e10238
Alquen Antonio Sarmiento <sarmientoalquen@gmail.com>
2024-04-11 23:23:49 +0800

    call proper lib func

--- app/dashboard/itinerary-builder/components/generate-and-save.tsx
+++ app/dashboard/itinerary-builder/components/generate-and-save.tsx
@@ -1,35 +1,80 @@
 "use client";
-import React, { FormEvent } from "react";
-import { ItineraryStage } from "./itinerary-stage-progress";
+
+import React, { FormEvent, useEffect, useState } from "react";
 import ItineraryMap from "@/components/itinerary-map";
+import { CompanyFilter } from "../lib/filter-companies";
+import {
+  CompanyWithAddress,
+  findNearestLocations,
+} from "../lib/create-itinerary";
+import FirebaseServicesInitialization from "@/lib/firebase/firebase_services_initialization";
+import { Firestore, getDoc, doc } from "firebase/firestore";
 
 const GenerateAndSave = ({
-  currentPageHandler,
+  startDate,
+  endDate,
+  address,
+  latLng,
+  filteredCompanies,
 }: {
-  currentPageHandler: CallableFunction;
+  startDate: Date;
+  endDate: Date;
+  address: string | null;
+  latLng: { lat: number; lng: number } | null;
+  filteredCompanies: CompanyFilter[] | null;
 }) => {
-  // const [date, setDate] = useState<{
-  //   startDate: Date;
-  //   endDate: Date;
-  // } | null>(null);
-  // const [country, setCountry] = useState<string | null>(null);
-  // const [state, setState] = useState<string | null>(null);
-  // const [address, setAddress] = useState<string | null>(null);
-  // const [latLng, setLatLng] = useState<{
-  //   lat: number;
-  //   lng: number;
-  // } | null>(null);
+  const db = FirebaseServicesInitialization.db as Firestore;
+  const [companies, setCompanies] = useState<CompanyWithAddress[]>([]);
+  const [sortedCompanies, setSortedCompanies] = useState<CompanyWithAddress[]>(
+    []
+  );
+
+  // Access the companies collection and get address.
+  useEffect(() => {
+    filteredCompanies?.forEach(async (company) => {
+      const docRef = doc(
+        db,
+        "companiesAddresses",
+        company.companyAddressUuid[0]
+      );
+      const docSnapshot = await getDoc(docRef);
+      if (docSnapshot.exists()) {
+        setCompanies((prev) => [
+          ...prev,
+          {
+            uuid: company.uuid,
+            address: docSnapshot.data().addressText,
+            latitude: docSnapshot.data().coordinates.latitude,
+            longitude: docSnapshot.data().coordinates.longitude,
+          },
+        ]);
+      }
+    });
+  }, [db, filteredCompanies]);
+
+  useEffect(() => {
+    if (!latLng) return;
+    findNearestLocations(latLng.lat, latLng.lng, companies).then(
+      (nearestCompanies) => setSortedCompanies(nearestCompanies)
+    );
+  }, [companies, latLng]);
+
+  if (!db) throw new Error("Firestore not initialized");
+  if (!latLng) throw new Error("No location selected");
 
   const handleFormSubmit = (event: FormEvent) => {
     event.preventDefault();
-    currentPageHandler(ItineraryStage.FILTER_COMPANIES);
+    // Save to Firestore
   };
 
   return (
     <form action="#" onSubmit={handleFormSubmit}>
       <div className="flex justify-center">
         <div className="mb-4 h-[500px] w-[800px] space-y-4 xl:col-span-3">
-          <ItineraryMap />
+          <ItineraryMap
+            startLatLng={latLng}
+            nearestCompanies={sortedCompanies}
+          />
         </div>
       </div>
     </form>
